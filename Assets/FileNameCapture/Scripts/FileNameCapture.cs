2bcb17191b2c5b61faea9b06be5c632f7ce61602
MirzkisD1Ex0 <lovezcz13685521730@live.com>
2020-11-27 23:09:36 +0800

    1

---
+++ Assets/FileNameCapture/Scripts/FileNameCapture.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.IO;
+
+/// <summary>
+/// 获取某个目录下指定类型的文件名
+/// </summary>
+public class FileNameCapture : MonoBehaviour
+{
+    public string[] Files; // 储存文件名的数组
+
+    private string fileStoragePath = Application.streamingAssetsPath + "/" + "VideoStorage"; // 文件路径
+    private string fileSuffix = ".mp4"; // 文件后缀名
+
+    private void Start()
+    {
+        GetFileName(fileStoragePath, fileSuffix, ref Files);
+    }
+
+    /// <summary>
+    /// 获取路径下全部指定类型的文件名
+    /// </summary>
+    /// <param name="path">路径</param>
+    /// <param name="suffix">后缀名</param>
+    /// <param name="files">用以存储文件名的数组</param>
+    private void GetFileName(string path, string suffix, ref string[] files)
+    {
+        if (!Directory.Exists(path)) // 如果路径不存在 // 返回 0
+        {
+            return;
+        }
+        DirectoryInfo directoryInfo = new DirectoryInfo(path); // 获取文件信息
+        FileInfo[] fileInfos = directoryInfo.GetFiles("*", SearchOption.AllDirectories);
+        #region 获取文件数组尺寸
+        int arraySize = 0;
+        for (int i = 0; i < fileInfos.Length; i++)
+        {
+            if (fileInfos[i].Name.EndsWith(fileSuffix))
+            {
+                arraySize++;
+                continue;
+            }
+        }
+        #endregion
+        files = new string[arraySize]; // 新建数组
+        #region 将符合要求的文件名存至数组
+        int arrayIndex = 0;
+        for (int i = 0; i < fileInfos.Length; i++)
+        {
+            if (fileInfos[i].Name.EndsWith(fileSuffix))
+            {
+                files[arrayIndex++] = fileInfos[i].Name; // 把符合要求的文件名存储至数组中
+                continue;
+            }
+        }
+        #endregion
+        return;
+    }
+}
