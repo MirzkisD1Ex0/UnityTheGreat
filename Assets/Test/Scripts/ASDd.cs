dbbb825aae2bd6835a80b12c482979183a2af706
MirzkisD1Ex0 <lovezcz13685521730@live.com>
2020-11-27 23:26:35 +0800

    2

---
+++ Assets/Test/Scripts/ASDd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASDd : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right;
+        }
+    }
+}
