3b1b76f9860ae1e55bec0d82240594d3217a1cca
MirzkisD1Ex0 <lovezcz13685521730@live.com>
2020-12-15 11:00:41 +0800

    fu

--- Assets/Json/Scripts/JsonConstructer.cs
+++ Assets/Json/Scripts/JsonConstructer.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 using Newtonsoft.Json;
 
+/// <summary>
+/// 
+/// </summary>
 public class JsonConstructer : MonoBehaviour
 {
-    #region DataClass
+    #region 数据类
     private class Packages
     {
         public string city;
@@ -41,13 +44,33 @@ public class JsonConstructer : MonoBehaviour
         #region 组建Json内容
         Packages jsonContent = new Packages();
         jsonContent.city = "上海";
-        jsonContent.id = 202001;
+        jsonContent.id = 2020001;
         jsonContent.info = new Info();
         jsonContent.info.weather = "晴";
+        jsonContent.info.position = "昆山南,嘉兴北";
         #endregion
 
-        string tempJson = JsonConvert.SerializeObject(jsonContent); // 将类序列化
-        byte[] byteStreaming = Encoding.UTF8.GetBytes(tempJson); // 把序列化后的类转为UTF8比特流
-        // JsonConvert;
+        string tempJson = JsonConvert.SerializeObject(jsonContent); // 将类序列化 // Newtonsoft.Json
+        byte[] jsonStreaming = Encoding.UTF8.GetBytes(tempJson); // 把序列化后的类转为UTF8比特流
+
+        SendJsonToSomeWhere(jsonHead, jsonStreaming);
+    }
+
+    /// <summary>
+    /// 把Json发粗去
+    /// </summary>
+    /// <param name="head"></param>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    private IEnumerator SendJsonToSomeWhere(Dictionary<string, string> head, byte[] content)
+    {
+        WWW www = new WWW("https://github.com", content, head); // 向HTTP服务器提交Post数据
+        yield return www;
+        if (www.error != null)
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+        Debug.Log(www.text);
     }
 }
